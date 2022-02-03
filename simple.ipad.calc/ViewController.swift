fbe4da03e54767727c2059aa05ac3792e1b7b225
VS <sergeevrov@icloud.com>
2022-02-03 22:51:28 +0300

    In the begining
    Started to make pet app for my old iPad 2. For now only pre-design was made: added digits, action buttons on main screen

---
+++ simple.ipad.calc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  simple.ipad.calc
+//
+//  Created by Victor Sergeev on 03.02.2022.
+//  Copyright © 2022 Victor Sergeev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ simple.ipad.calcTests/simple_ipad_calcTests.swift
@@ -0,0 +1,34 @@
+//
+//  simple_ipad_calcTests.swift
+//  simple.ipad.calcTests
+//
+//  Created by Victor Sergeev on 03.02.2022.
+//  Copyright © 2022 Victor Sergeev. All rights reserved.
+//
+
+import XCTest
+@testable import simple_ipad_calc
+
+class simple_ipad_calcTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
