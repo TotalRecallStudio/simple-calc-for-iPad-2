c981b70cb221658657d66bd8b386b449ce76d6f1
VS <sergeevrov@icloud.com>
2022-02-05 19:23:40 +0300

    Redesigned IU
    Redesigned UI, taged 'numbers', sent for to continue at MacMini

--- simple.ipad.calc/ViewController.swift
+++ simple.ipad.calc/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func numbers(_ sender: UIButton)
+    {
+        
+    }
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
